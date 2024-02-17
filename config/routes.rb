8ac1d8a4b91d631e15827ffee25d05f89b180aa0
princessjoy22 <princessjoyibarretapopanes@gmail.com>
2024-02-17 10:22:58 +0800

    once applied this commit will add a root path

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tasks#index"
   get '/tasks', to: 'tasks#index', as: :tasks
   get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
