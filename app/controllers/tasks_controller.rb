c5e8aaf3032c9511cc9ad534216f5ff7a6085721
princessjoy22 <princessjoyibarretapopanes@gmail.com>
2024-02-19 16:27:56 +0800

    Once applied,the CSS will be implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy, :mark_as_done]
+    before_action :all_task, only: [:index, :new, :show, :edit]
 
     def index
         @tasks = Task.all
@@ -9,7 +10,7 @@ class TasksController < ApplicationController
     def mark_as_done
         if @task
             @task.update(completed: true)
-            redirect_to tasks_path, notice: "Task marked as done successfully!"
+            redirect_to tasks_path, notice: "Task done!"
         else
             redirect_to tasks_path, notice: "Task not found"
         end
@@ -28,7 +29,7 @@ class TasksController < ApplicationController
         if @task.save
             redirect_to tasks_path, notice: "Task added!"
         else
-            render :new
+            redirect_to new_task_path, notice: "No added task."
         end
     end
 
@@ -45,18 +46,21 @@ class TasksController < ApplicationController
 
     def destroy
         @task.destroy
-
         redirect_to tasks_path, notice: "Task successfully deleted." 
     end
 
     private
 
     def task_params
-        params.require(:task).permit(:task_name)
+        params.require(:task).permit(:task_name, :body)
     end
 
     def set_task
         @task = Task.find(params[:id])
+    end
+
+    def all_task
+        @tasks = Task.all
     end
 
 end
